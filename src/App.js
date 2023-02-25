f164832a2399b69f32816c7103ca18407ef5181e
joachimgro <gjoachim112@gmail.com>
2023-02-25 20:51:46 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 import {
   selectIsConnectedToRoom,
   useHMSActions,
-  useHMSStore
-} from "@100mslive/react-sdk";
-import './App.css';
-import SignIn from './SignIn';
-import Conference from './Conference';
-import Footer from './Footer';
-
-
+  useHMSStore,
+} from '@100mslive/react-sdk'
+import './App.css'
+import SignIn from './SignIn'
+import Conference from './Conference'
+import Footer from './Footer'
 
 function App() {
-
-  const isConnected = useHMSStore(selectIsConnectedToRoom);
-  const hmsActions = useHMSActions();
+  const isConnected = useHMSStore(selectIsConnectedToRoom)
+  const hmsActions = useHMSActions()
 
   useEffect(() => {
-      window.onunload = () => {
-          if(isConnected) {
-            hmsActions.leave();
-          }
-        }
-      }, [hmsActions, isConnected]);
+    window.onunload = () => {
+      if (isConnected) {
+        hmsActions.leave()
+      }
+    }
+  }, [hmsActions, isConnected])
 
   return (
     <div className="App">
       {isConnected ? (
         <>
-          <Conference />
+          {/* <Conference /> */}
           <Footer />
         </>
-      ):(
+      ) : (
         <SignIn />
       )}
     </div>
-  );
-
+  )
 }
 
-export default App;
+export default App
--- src/Footer.js
+++ src/Footer.js
@@ -1,100 +1,94 @@
-import React from "react";
-import { useAVToggle } from "@100mslive/react-sdk";
+import React from 'react'
+import { useAVToggle } from '@100mslive/react-sdk'
 import {
-    selectIsConnectedToRoom,
-    useHMSActions,
-    useHMSStore,
-    selectPeers,
-  } from "@100mslive/react-sdk";
+  selectIsConnectedToRoom,
+  useHMSActions,
+  useHMSStore,
+  selectPeers,
+} from '@100mslive/react-sdk'
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMicrophoneSlash, faMicrophone, faVideo, faVideoSlash, 
-         faUserPlus, faArrowUpRightFromSquare, faMessage, faFaceSmile, faGear, faEllipsis
-} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faMicrophoneSlash,
+  faMicrophone,
+  faVideo,
+  faVideoSlash,
+  faUserPlus,
+  faArrowUpRightFromSquare,
+  faMessage,
+  faFaceSmile,
+  faGear,
+  faEllipsis,
+} from '@fortawesome/free-solid-svg-icons'
 
 function Footer() {
+  const peers = useHMSStore(selectPeers)
 
-    const peers = useHMSStore(selectPeers);
+  const userCount = peers.length
 
-    const userCount = peers.length;
+  const isConnected = useHMSStore(selectIsConnectedToRoom)
+  const hmsActions = useHMSActions()
 
-    const isConnected = useHMSStore(selectIsConnectedToRoom);
-    const hmsActions = useHMSActions();
-
-    const {isLocalAudioEnabled,
+  const {
+    isLocalAudioEnabled,
     isLocalVideoEnabled,
     toggleAudio,
-    toggleVideo
-    } = useAVToggle();
+    toggleVideo,
+  } = useAVToggle()
 
-    return (
-        <div className="main_control">
-            <div className="main_controls_section">
-                <div className="main_controls_button" onClick={toggleAudio}>
-                {isLocalAudioEnabled ? (
-                <><FontAwesomeIcon icon={faMicrophone} size="lg" /><span className="button_name">Mute</span></>
-                
-                ): (
-                <><FontAwesomeIcon icon={faMicrophoneSlash} size="lg" /><span className="button_name">Unmute</span></>
-                )} 
-                </div>
-
-                <div className="main_controls_button" onClick={toggleVideo}>
-                {isLocalVideoEnabled ? (
-                <><FontAwesomeIcon icon={faVideo} size="lg" /><span className="button_name">Stop Video</span></>
-                ): (
-                <><FontAwesomeIcon icon={faVideoSlash} size="lg" /><span className="button_name">Start Video</span></>
-                )} 
-                </div>
-
+  return (
+    <div className="main_control_bg">
+      <div className="main_control">
+        <div className="main_controls_section">
+          <div className="main_controls_button">
+            <div>
+              <FontAwesomeIcon icon={faUserPlus} size="4x" color="#A24936" />
+              <>{userCount}</>
             </div>
-
-            <div className="main_controls_section">
-                <div className="main_controls_button">
-                    <div>
-                    <FontAwesomeIcon icon={faUserPlus} size="lg" /><>{userCount}</>
-                    </div>
-                    <span className="button_name">Participants</span>
-                </div>
-                <div className="main_controls_button">
-                    <FontAwesomeIcon icon={faArrowUpRightFromSquare}  size="lg" color="" />
-                    <span className="button_name">Share Screen</span>
-                </div>
-                <div className="main_controls_button">
-                    <FontAwesomeIcon icon={faMessage} size="lg" />
-                    <span className="button_name">Chat</span>
-                </div>
-                <div className="main_controls_button">
-                    <FontAwesomeIcon icon={faFaceSmile} size="lg"/>
-                    <span className="button_name">Reactions</span>
-                </div>
-                <div className="main_controls_button">
-                    <FontAwesomeIcon icon={faGear} size="lg"/>
-                    <span className="button_name">Settings</span>
-                </div>
-                <div className="main_controls_button">
-                    <FontAwesomeIcon icon={faEllipsis} size="lg" />
-                    <span className="button_name">More</span>
-                </div>
-            </div>
-
-            <div className="main_controls_section">
-              <div className="main_controls_button">
-                    {isConnected && (
-                        <button
-                        id="leave-btn"
-                        class="btn-danger"
-                        onClick={() => hmsActions.leave()}
-                        >
-                        Leave
-                        </button>
-                    )}
-             </div>
-            </div>
-            
+            <span className="button_name">Participants</span>
+          </div>
         </div>
-        
-    )
-}   
 
-export default Footer;
+        <div className="main_controls_section">
+          <div className="main_controls_button" onClick={toggleAudio}>
+            {isLocalAudioEnabled ? (
+              <>
+                <FontAwesomeIcon
+                  icon={faMicrophone}
+                  size="4x"
+                  color="#A24936"
+                />
+                <span className="button_name">Mute</span>
+              </>
+            ) : (
+              <>
+                <FontAwesomeIcon
+                  icon={faMicrophoneSlash}
+                  size="4x"
+                  color="#A24936"
+                />
+                <span className="button_name">Unmute</span>
+              </>
+            )}
+          </div>
+        </div>
+
+        <div className="main_controls_section">
+          <div className="main_controls_button">
+            {isConnected && (
+              <button
+                id="leave-btn"
+                class="btn-danger"
+                onClick={() => hmsActions.leave()}
+              >
+                Leave
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
